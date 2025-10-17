2858433dd70cf843d8170910b360dce2e13947ac
Ch-Pratibha <pratibhachatre1@gmail.com>
2025-10-18 01:04:58 +0530

    commit

--- factorial.sh
+++ factorial.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-calculate_factorial(){
+calculate_Factorial(){
 num=$1
 fact=1
 for(( i=1; i<=num; i++ )); do
@@ -7,8 +6,7 @@ fact=$((fact*i))
 done
 echo $fact
 }
-
-echo "Enter a number:"
+echo "Enter a number: "
 read input_num
-factorial_result=$( calculate_factorial $input_num )
-echo "Factorial of $input_num is: $factorial_result"
+factorial_result=$( calculate_Factorial $input_num )
+echo "Factorial of a number $input_num is: $factorial_result"
